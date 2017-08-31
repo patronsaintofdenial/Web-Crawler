1805a6d61aee7289f6613718ea87fa80327b737c
Siddhant Bagga <30875228+patronsaintofdenial@users.noreply.github.com>
2017-09-01 00:20:33 +0530

    Update domain.py

--- domain.py
+++ domain.py
@@ -1,24 +1,16 @@
-from html.parser import HTMLParser
-from urllib import parse
+from urllib.parse import urlparse
 
-class LinkFinder(HTMLParser):
-    def __init__(self, base_url, page_url):
-        super().__init__()
-        self.base_url=base_url
-        self.page_url=page_url
-        self.links=set()
 
- # When we call HTMLParser feed() this function is called when it encounters an opening tag <a>
+#Get Domain Name
 
-    def handle_start_tag(self, tag, attrs):
-            if tag=='a':
-                for(attribute,value) in attrs:
-                    if attribute=='href':
-                        url=parse.urljoin(self.base_url, value)
-                        self.links.add(url)
-
-    def page_links(self):
-        return self.links
-
-    def error(self, message):
-            pass
+def get_domain_name(url):
+    try:
+        results=get_sub_Domain_name(url).split('.')
+        return results[-2]  + '.' + results[-1]
+    except:
+        return ''
+def get_sub_Domain_name(url):
+    try:
+        return urlparse(url).netloc
+    except:
+        return ''
